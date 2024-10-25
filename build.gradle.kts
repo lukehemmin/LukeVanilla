d3f83b1d198958ffe54cf32e7af0f7880df556e1
박상호 Sangho Park <ps040211@gmail.com>
2024-10-25 18:39:14 +0900

    papermc 1.21.1 업데이트와 종속성 HikariCP 추가
    Papermc API 1.21 -> 1.21.1
    HikariCP 5.0.1 종속성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,8 +17,9 @@ repositories {
 }
 
 dependencies {
-    compileOnly("io.papermc.paper:paper-api:1.21-R0.1-SNAPSHOT")
+    compileOnly("io.papermc.paper:paper-api:1.21.1-R0.1-SNAPSHOT")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("com.zaxxer:HikariCP:5.0.1")
 }
 
 val targetJavaVersion = 21
