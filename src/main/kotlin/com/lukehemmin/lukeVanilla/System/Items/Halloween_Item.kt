ab9e9cc52f63670cfba5c417ae3a63e55e65d72f
박상호 Sangho Park <ps040211@gmail.com>
2024-11-03 16:16:56 +0900

    커스텀 아이템 할로윈 랜턴 설치를 하지 못하도록 합니다.
    halloween_lentern 이 설치되지 않도록 코드를 작성하였습니다.
    
    이는 oraxen 종속성이 필요합니다.

---
+++ src/main/kotlin/com/lukehemmin/lukeVanilla/System/Items/Halloween_Item.kt
@@ -0,0 +1,26 @@
+package com.lukehemmin.lukeVanilla.System.Items
+
+import io.th0rgal.oraxen.api.OraxenItems
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.block.BlockPlaceEvent
+import org.bukkit.inventory.ItemStack
+
+class Halloween_Item : Listener {
+    @EventHandler
+    fun onBlockPlace(event: BlockPlaceEvent) {
+        val item = event.itemInHand
+
+        // Oraxen 아이템인지 확인
+        if (OraxenItems.exists(item)) {
+            // 아이템의 ID 가져오기
+            val oraxenId = OraxenItems.getIdByItem(item)
+
+            // halloween_lentern 아이템인지 확인
+            if (oraxenId == "halloween_lentern") {
+                event.isCancelled = true
+                event.player.sendMessage("§c이 아이템은 설치할 수 없습니다. 설명을 확인해주세요!")
+            }
+        }
+    }
+}
