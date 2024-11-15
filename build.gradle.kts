19b022832f45b54cd95a14d4b505979fa5689312
박상호 Sangho Park <ps040211@gmail.com>
2024-11-15 21:35:07 +0900

    JDA 5.2.1 로 업데이트 및 emoji-java 추가
    JDA를 5.2.0 에서 5.2.1로 업데이트 하였습니다.
    implementation("net.dv8tion:JDA:5.2.0") -> implementation("net.dv8tion:JDA:5.2.1")
    emoji-java 를 종속성을 추가하였습니다.
    implementation ("com.vdurmont:emoji-java:5.1.1")

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,8 +21,9 @@ dependencies {
     compileOnly("io.papermc.paper:paper-api:1.21.1-R0.1-SNAPSHOT")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("com.zaxxer:HikariCP:5.0.1")
+    implementation ("com.vdurmont:emoji-java:5.1.1")
     compileOnly("io.th0rgal:oraxen:1.183.0")
-    implementation("net.dv8tion:JDA:5.2.0")
+    implementation("net.dv8tion:JDA:5.2.1")
     //implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
 }
 
