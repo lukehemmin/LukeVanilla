3e661a804f5e538ee6c3220625103f11b0ebb21d
박상호 Sangho Park <ps040211@gmail.com>
2024-11-15 14:41:35 +0900

    Join_Quit_Message 의 기본값 추가.
    Join_Quit_Message 테이블 생성시 기본값을 추가하여 쉽게 수정할 수 있도록 변경.

--- src/main/kotlin/com/lukehemmin/lukeVanilla/System/Database/DatabaseInitializer.kt
+++ src/main/kotlin/com/lukehemmin/lukeVanilla/System/Database/DatabaseInitializer.kt
@@ -71,6 +71,7 @@ class DatabaseInitializer(private val database: Database) {
     private fun createJoinQuitMessageTable() { // 입장, 퇴장 메시지 테이블
         database.getConnection().use { connection ->
             val statement = connection.createStatement()
+            // 테이블 생성
             statement.executeUpdate(
                 "CREATE TABLE IF NOT EXISTS Join_Quit_Message (" +
                         "service_type VARCHAR(30) NOT NULL, " +
@@ -79,6 +80,38 @@ class DatabaseInitializer(private val database: Database) {
                         "PRIMARY KEY (service_type, message_type)" +
                         ")"
             )
+
+            // 테이블이 비어 있는지 확인
+            val resultSet = statement.executeQuery("SELECT COUNT(*) AS count FROM Join_Quit_Message")
+            var isEmpty = true
+            if (resultSet.next()) {
+                isEmpty = resultSet.getInt("count") == 0
+            }
+
+            // 테이블이 비어 있다면 기본값 삽입
+            if (isEmpty) {
+                val insertStmt = connection.prepareStatement(
+                    "INSERT INTO Join_Quit_Message (service_type, message_type, message) VALUES (?, ?, ?)"
+                )
+
+                // 기본값 데이터 리스트
+                val defaultMessages = listOf(
+                    Triple("Lobby", "LobbyJoinMessage", "§b§l{playerName} §a§l님, 이곳은 로비서버에요!\n\n§a§l이곳은 서버 점검, 패치가 있을때 서버가 다시 열릴때까지 기다리는 서버에요!\n§a§l서버가 열리면 다시 원래 서버, 원래 위치로 돌아가게 되요!\n§a§l잠시만 기다려주세요! ( 예정시간은 공지사항이나 패치노트에 있어요! )"),
+                    Triple("Lobby", "LobbyServerJoin", "       §f§l[§a§l+§f§l] §f§l{playerName} 님이 로비 서버에 접속했습니다!"),
+                    Triple("Lobby", "LobbyServerQuit", "       §f§l[§c§l-§f§l] §f§l{playerName} 님이 로비 서버에서 나갔습니다!"),
+                    Triple("Vanilla", "VanillaJoinMessage", "            §b§l{playerName} §a§l님, 오늘도 서버에 오셨네요! 반가워요!\n"),
+                    Triple("Vanilla", "VanillaServerFirstJoin", "       §f§l[§e§l++§f§l] §f§l{playerName} 님이 처음으로 서버에 접속했습니다!"),
+                    Triple("Vanilla", "VanillaServerJoin", "       §f§l[§a§l+§f§l] §f§l{playerName} 님이 서버에 접속했습니다!"),
+                    Triple("Vanilla", "VanillaServerQuit", "       §f§l[§c§l-§f§l] §f§l{playerName} 님이 서버에서 나갔습니다!")
+                )
+
+                for ((serviceType, messageType, message) in defaultMessages) {
+                    insertStmt.setString(1, serviceType)
+                    insertStmt.setString(2, messageType)
+                    insertStmt.setString(3, message)
+                    insertStmt.executeUpdate()
+                }
+            }
         }
     }
 
